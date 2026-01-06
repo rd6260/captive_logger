1766ac2a8164ae92176594d0be7a67dc0813f4a4
rd <rohandas.zero@gmail.com>
2026-01-06 12:32:52 +0530

    update

--- autologger.py
+++ autologger.py
@@ -1,23 +1,43 @@
+#!/usr/bin/python
+
 import requests
+# import subprocess
+import xml.dom.minidom
 
-USERNAME = ""
-PASSWORD = ""
+USERNAME = "24BDS051"
+PASSWORD = "1!Quantum"
+
+#  this is example data. fill it up with your ids :)
+data = {
+        "example": ["27BDS001", "Abc@123"],
+        }
 
 
-
+# profile = subprocess.run(
+#         ["fzf", f"{keys}" for ]
+#         )
 url = "http://172.16.16.16:8090/httpclient.html"
+user = "example"
 payload = {
     "mode": "191",
-    "username": USERNAME,
-    "password": PASSWORD,
+    "username": data[user][0],
+    "password": data[user][1],
     "a": "1551345153461"
 }
 
 response = requests.post(url, data=payload)
 
+def pretty_print(xml_string):
+    # Parse the string
+    dom = xml.dom.minidom.parseString(xml_string)
+    # Pretty print with indentation
+    pretty_xml = dom.toprettyxml(indent="  ")
+    print(pretty_xml)
+
 if response.ok:
     print("Request successful!")
-    print("Response:", response.text)
+    # print("Response:", response.text)
+    pretty_print(response.text.replace("username", user))
 else:
     print("Request failed!")
     print("Status Code:", response.status_code)
